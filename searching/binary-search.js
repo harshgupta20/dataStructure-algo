91c429f1bb889848af116a5a7f1b036416cb963b
Harsh Gupta <59175412+harshgupta20@users.noreply.github.com>
2023-02-25 09:55:55 +0000

    Binary search

--- searching/binary-search.js
+++ searching/binary-search.js
@@ -1,3 +1,30 @@
 console.log("working");
 
-let userInput = 5;
+let userInput = 1;
+let array = [2,3,5,6,7,8,9];
+
+console.log(checkNum(userInput, array));
+
+function checkNum(userInput, array){
+    
+    let start = 0;
+    let end = array.length-1;
+    let mid = (end - start)/2; 
+
+    while(start<=end && start<0){
+        if(userInput < array[mid]){
+            console.log("uper"+mid);
+            end = mid+1;
+            mid = (end+start)/2;
+        }
+        else if(userInput>array[mid]){
+            console.log("niche"+mid);
+            start = mid+1;
+            mid=(end+start)/2;
+        }
+        else{
+            return "Element found on index : "+mid;
+        }
+    }
+    return "Not Found ";
+}
