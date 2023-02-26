0ac54d1c5f7b28f46e27dcb6872daae47ba89c90
Harsh Gupta <59175412+harshgupta20@users.noreply.github.com>
2023-02-26 11:06:30 +0000

    Minor check added

--- sorting/insertion-sort.js
+++ sorting/insertion-sort.js
@@ -1,18 +1,21 @@
 console.log("working");
 
 
-let array = [10,1,7,4,8,2,11];
+// let array = [10,1,7,4,8,2,11];
+let array = [1,2,3,4,5,6,7];
 
+let bigOh = 0;
 let arraySorted = insertionSort(array);
-
 console.log(arraySorted);
 
 
 
 function insertionSort(arr){
     for(let i=1; i<arr.length;i++){
+        bigOh++;
         let tempNum = arr[i];
         for(let j=i-1; j>=0; j--){
+            bigOh++;
             if(arr[j] > tempNum){
                 arr[j+1] = arr[j];
                 arr[j]=tempNum;
@@ -22,6 +25,7 @@ function insertionSort(arr){
         }
     }
 
+    console.log("BigOh :" +bigOh)
     return arr;
 }
 
