4c720cd59572420d1389bc672d4f4013fd45c0bb
Harsh Gupta <59175412+harshgupta20@users.noreply.github.com>
2023-02-26 09:50:40 +0000

    Minor logic build on insertion sort

---
+++ sorting/insertion-sort.js
@@ -0,0 +1,19 @@
+console.log("working");
+
+
+let array = [1,2,3,4,65];
+
+let arraySorted = insertionSort(array);
+
+console.log(arraySorted);
+
+
+function insertionSort(arr){
+    for(let i=arr.length-1; i>=1; i++){
+        for(let j=0; j<=arr.length-i; j++){
+            if(arr[j]>arr[j+1]){
+                
+            }
+        }
+    }
+}
