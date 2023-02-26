38b4b04370472dfb99757220f303fea70078f7d4
Harsh Gupta <59175412+harshgupta20@users.noreply.github.com>
2023-02-26 11:00:34 +0000

    insertion sort

--- sorting/insertion-sort.js
+++ sorting/insertion-sort.js
@@ -1,19 +1,46 @@
 console.log("working");
 
 
-let array = [1,2,3,4,65];
+let array = [10,1,7,4,8,2,11];
 
 let arraySorted = insertionSort(array);
 
 console.log(arraySorted);
 
 
+
 function insertionSort(arr){
-    for(let i=arr.length-1; i>=1; i++){
-        for(let j=0; j<=arr.length-i; j++){
-            if(arr[j]>arr[j+1]){
-                
+    for(let i=1; i<arr.length;i++){
+        let tempNum = arr[i];
+        for(let j=i-1; j>=0; j--){
+            if(arr[j] > tempNum){
+                arr[j+1] = arr[j];
+                arr[j]=tempNum;
+            }else{
+                break;
             }
         }
     }
+
+    return arr;
 }
+
+
+
+
+
+
+
+
+
+
+
+// function insertionSort(arr){
+//     for(let i=arr.length-1; i>=1; i++){
+//         for(let j=0; j<=arr.length-i; j++){
+//             if(arr[j]>arr[j+1]){
+
+//             }
+//         }
+//     }
+// }
